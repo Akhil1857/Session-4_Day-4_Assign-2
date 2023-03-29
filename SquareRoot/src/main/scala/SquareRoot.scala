893be091b6225a211b090bcfaeea91a42336b767
Akhil1857 <125340453+Akhil1857@users.noreply.github.com>
2023-03-29 23:03:58 +0530

    Update SquareRoot.scala

--- SquareRoot/src/main/scala/SquareRoot.scala
+++ SquareRoot/src/main/scala/SquareRoot.scala
@@ -1,6 +1,8 @@
 import scala.util.Try
 
 class SquareRoot {
+  
+  //This squareRootCalculator Calculates the square root of each element of the list
   def squareRootCalculator(numberList: List[Int]): List[Try[Int]] = {
     numberList.map { value =>
       Try {
